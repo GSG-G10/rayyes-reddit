48651432a79cc8932d1c8ad66c0870aefc9a8e79
mohammed Rayyes <mod.rayyes@gmail.com>
2021-09-10 23:42:24 +0300

    set database reject unauthorized to false

--- server/database/connection.js
+++ server/database/connection.js
@@ -6,7 +6,7 @@ const connectionString = process.env.DB_URL;
 const pool = new Pool({
   connectionString,
   max: process.env.DB_MAX_CONNECTIONS || 2,
-  ssl: process.env.NODE_ENV === 'production'
+  ssl: { rejectUnauthorized: false }
 });
 
 module.exports = pool;
