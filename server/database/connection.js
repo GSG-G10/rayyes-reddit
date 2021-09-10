7447ad6a95ef1c121d29aa381fb665ed074b0a23
mohammed Rayyes <mod.rayyes@gmail.com>
2021-09-10 19:59:54 +0300

    database edit 2

--- server/database/connection.js
+++ server/database/connection.js
@@ -1,11 +1,47 @@
 require('dotenv').config();
-const { Pool } = require('pg');
 
-const connectionString = process.env.DB_URL;
+const {
+  Pool,
+} = require('pg');
 
-const pool = new Pool({
-  connectionString,
-  max: process.env.DB_MAX_CONNECTIONS || 2
+const {
+  NODE_ENV,
+  DB_URL,
+  DEV_DB_URL,
+  TEST_DB_URL,
+} = process.env;
+
+let dbUrl = '';
+
+switch (NODE_ENV) {
+  case 'production':
+    dbUrl = DB_URL;
+    break;
+  case 'development':
+    dbUrl = DEV_DB_URL;
+    break;
+  case 'test':
+    dbUrl = TEST_DB_URL;
+    break;
+  default:
+    throw new Error('No Database URL');
+}
+
+module.exports = new Pool({
+  connectionString: dbUrl,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
-module.exports = pool;
+// require('dotenv').config();
+// const { Pool } = require('pg');
+
+// const connectionString = process.env.DB_URL;
+
+// const pool = new Pool({
+//   connectionString,
+//   max: process.env.DB_MAX_CONNECTIONS || 2
+// });
+
+// module.exports = pool;
